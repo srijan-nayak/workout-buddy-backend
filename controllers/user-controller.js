7258154299a1d567e14a0517d9859f70b9343909
Srijan Nayak <srijannayak0@gmail.com>
2023-06-17 19:41:24 +0530

    Send back token for successful login

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,14 @@ const createToken = (id) => {
 };
 
 const loginUser = async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const signupUser = async (req, res) => {
