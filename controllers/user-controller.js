17e7f2d362988e08718f004b61fb195a9dfa456c
Srijan Nayak <srijannayak0@gmail.com>
2023-06-17 19:34:38 +0530

    Create JWTs for authentication

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,9 @@
 const UserModel = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ _id: id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
   res.json({ message: "login user" });
@@ -8,7 +13,8 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await UserModel.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
